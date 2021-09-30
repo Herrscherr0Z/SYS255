f813ad7971c77c08f1a1ab294ce6db83ec4babca
Joshua Leon Mejia (adm) <joshua.leonmejia-adm@joshua.local>
2021-09-30 07:59:09 -0700

    third commit

---
+++ Week05/class/WMI.ps1
@@ -0,0 +1,14 @@
+# Use the Get-WMIObject cmdlet
+# Get-wmiobject -Class win32_service | Select Name, Path, ProcessID
+
+# Get-WmiObject -list | where { $_.Name -ilike "win32_[n-z]*" } | sort-object
+
+# Get-WmiObject -Class win32_account | Get-Member
+
+# Task: Grab the network adapter information using the WMI class
+# Get the IP, default gateway, and DNS servers
+# BONUS: get the DHCP server
+# post code to GitHub
+# Running your code using a screen recording
+
+Get-WMIObject -Class Win32_NetworkAdapterConfiguration | select IPAddress, DefaultIPGateway, DNSServerSearchOrder, DHCPServer
