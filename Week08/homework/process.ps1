231172c5d8bfcfb65acd87ab42e0b52c19304bcc
Herrscherr0Z <71039974+Herrscherr0Z@users.noreply.github.com>
2021-10-24 18:20:59 -0700

    Add files via upload

---
+++ Week08/homework/process.ps1
@@ -0,0 +1,24 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files
+
+$myDir = "C:\Users\joshua.leonmejia-adm\Desktop\"
+
+# list all the available Windows Event Logs
+Get-Eventlog -list
+
+# Create a prompt to allow user to select the log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above."
+
+# String Search Process Prompt
+$Sprompt = Read-Host -Prompt "Enter string to search."
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | Export-Csv -NoTypeInformation -Path "$myDir\securityLogs.csv"
+
+# | where {$_.Message -ilike "*$Sprompt*"}  | for some reason whenever I used this line of code it didnt output the file correctly for me and would output nothing into it
+# when I removed it everyhting worked as intended, I would like some more assistance on this since codeing isnt my strongest area 
+
+# Task: Create a prompt that allows the user to specify a keyword or phrase to search on.
+# Find a string from your event logs to search on
+
---
+++ Week08/homework/threatintell.ps1
@@ -0,0 +1,69 @@
+﻿# Array of websites containing threat intell
+$drop_urls = @('https://rules.emergingthreats.net/blockrules/emerging-botcc.rules','https://rules.emergingthreats.net/blockrules/compromised-ips.txt')
+
+
+# Loop through the URLs for the rules list
+foreach ($u in $drop_urls) {
+
+    # Extract the filename
+    $temp = $u.split("/")
+    
+    # The last element in the array plucked off is the filename
+    $file_name = $temp[-1]
+
+    if (Test-Path $file_name) {
+
+        continue
+
+    } else {
+
+        # Download the rules list
+        Invoke-WebRequest -Uri $u -OutFile $file_name
+
+
+    } # close if statement
+
+} # close the foreach loop
+
+# Array containing the filename
+$input_paths = @('compromised-ips.txt','.\emerging-botcc.rules')
+
+# Extract the IP addresses
+# 108.190.109.107
+# 108.191.2.72
+$regex_drop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+
+# Append the IP addresses to the temporary IP list
+Select-String -Path $input_paths -Pattern $regex_drop | `
+ForEach-Object { $_.Matches } | `
+ForEach-Object { $_.Value } | Sort-Object | Get-Unique | `
+Out-File -FilePath "ips-bad.tmp"
+
+# Get the IP addresses discovered, loop through and replace the behinning of the line with IPtables syntax
+# After the IP address, add the remaining IPtables syntax and save the results to a file
+# iptables -A INPUT -s 108.191.2.72 IP -j DROP 
+
+$ruleSet = ".\ips-bad.tmp"
+
+$message = read-host -prompt "Enter 0 for IPTables, 1 for Cisco or 2 for Windows Rulesets"
+
+Switch ($message) {
+    0 { (Get-Content -Path $ruleSet) | % `
+        { $_ -replace "^","iptables -A INPUT -s" -replace "$","-j DROP" } | `
+        Out-File -FilePath "iptables.bash" }
+
+        #Cisco Ruleset
+        1 { (Get-Content -Path $ruleSet) | % `
+        { $_ -replace "^","access-list 1 deny host" -replace "$" } | `
+        Out-File -FilePath "cisco.bash" }
+
+        # Windows Ruleset
+        2 { (Get-Content -Path $ruleSet) | % `
+        { $_ -replace "^","netsh advfirewall add rulename='IPBlock' dir=in interface=any action=block remoteip=" -replace "$","/32"} |`
+        Out-File -FilePath "windowsfirewall.bash" }
+
+    }
+
+
+
+
