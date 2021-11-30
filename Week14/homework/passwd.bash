f352091bbaa9082182fa0fe0c6511d513559d47a
Herrscherr0Z <71039974+Herrscherr0Z@users.noreply.github.com>
2021-11-30 17:54:44 -0500

    Update passwd.bash

--- Week14/homework/passwd.bash
+++ Week14/homework/passwd.bash
@@ -3,8 +3,8 @@
 # Script to parse file
 read -p "Enter the password file name:" PASSWORD
 
-awk -F : ' BEGIN { format = "%-19s %-9s %-9s %-9s %-75s %-30s %s\n"
-        printf format, "Username", "Unused", "User ID", "Group ID", "Comment", "Home Directory", "Default Shell"
-        printf format, "--------", "------", "-------", "--------", "-------", "--------------", "-------------" }
-      { printf format, $1, $2, $3, $4, $5, $6, $7 }
+awk -F: ' BEGIN { format = "%-18s %-9s %-9s %-65s %s\n"
+        printf format, "Username", "User ID", "Group ID", "Home Directory", "Default Shell"
+        printf format, "--------", "-------", "--------", "--------------", "-------------" }
+      { printf format, $1, $3, $4, $5, $6  }
 ' ${PASSWORD}
